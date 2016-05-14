71a1481dbe23723e0c0af6b5fcfb042c888107a0
rumbodeveloper <rumbo.developer@rumbo181.com>
2016-05-14 20:46:43 +0200

    mejorada la lista de rss

--- Headlines.py
+++ Headlines.py
@@ -19,13 +19,13 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol':'http://www.iol.co.za/cmlink/1.640',
-             'elmundo':"http://www.elmundo.com/images/rss/titulares",
-             'elpais':"http://feeds.feedburner.com/ElPaiscomco",}
+             'iol':'http://www.iol.co.za/cmlink/1.640',}
+
+
 
 @app.route('/')
 @app.route("/<publication>")
-def get_news(publication='elpais'):
+def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",
                            articles = feed['entries'][:5],
