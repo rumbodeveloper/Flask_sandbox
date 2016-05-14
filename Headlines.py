db5ecc59f76a031ad878b396e42073d94dbb390a
rumbodeveloper <rumbo.developer@rumbo181.com>
2016-05-14 20:57:05 +0200

    incorporadas nuevas rss

--- Headlines.py
+++ Headlines.py
@@ -19,13 +19,15 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol':'http://www.iol.co.za/cmlink/1.640',}
+             'iol':'http://www.iol.co.za/cmlink/1.640',
+             'elpais':'feed://ep00.epimg.net/rss/elpais/portada.xml',
+             'elmundo':'feed://estaticos.elmundo.es/elmundo/rss/portada.xml'}
 
 
 
 @app.route('/')
 @app.route("/<publication>")
-def get_news(publication='bbc'):
+def get_news(publication='elpais'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",
                            articles = feed['entries'][:5],
