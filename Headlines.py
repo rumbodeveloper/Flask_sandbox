49575bbcae73b24e9f5c1ce4a83974473a840fbe
rumbodeveloper <rumbo.developer@rumbo181.com>
2016-05-15 20:54:45 +0200

    Formulario para escoger el feed

--- Headlines.py
+++ Headlines.py
@@ -10,7 +10,7 @@
 #Date: '13/5/16'
 
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,15 +19,19 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol':'http://www.iol.co.za/cmlink/1.640',
-             'elpais':'feed://ep00.epimg.net/rss/elpais/portada.xml',
-             'elmundo':'feed://estaticos.elmundo.es/elmundo/rss/portada.xml'}
+             'elpais':'feed://ep00.epimg.net/rss/elpais/portada.xml',}
 
 
 
 @app.route('/')
 @app.route("/<publication>")
 def get_news(publication='elpais'):
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "elpais"
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",
                            articles = feed['entries'][:5],
