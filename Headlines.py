1f5d998316848270029b4fbccfb864a9087eb658
rumbodeveloper <rumbo.developer@rumbo181.com>
2016-05-16 17:40:26 +0200

    Primer uso de cookies

--- Headlines.py
+++ Headlines.py
@@ -10,9 +10,10 @@
 #Date: '13/5/16'
 
 from constants import OPENWHEATHER_API_KEY, OPENEXHANGE_API_KEY
+import datetime
 import json
 import feedparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import urllib2
 import urllib
 
@@ -37,33 +38,34 @@ DEFAULTS = {'publication': 'elpais',
 @app.route("/<publication>")
 def home():
     #obtener noticias de ultima hora customizadas
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
+
     #obtener el tiempo customizado
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
+
     #obtener un ratio de cambio customizado
-    currency_from = request.args.get('currency_from')
-    currency_to = request.args.get('currrency_to')
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
     rate,currencies = get_rates(currency_from,currency_to)
 
     #renderizamos la pagina
-    return render_template("home.html",
+    response = make_response(render_template("home.html",
                            articles=articles,
                            weather=weather,
                            currency_from=currency_from,
                            currency_to=currency_to,
                            rate=rate,
-                           currencies=sorted(currencies)
-                           )
+                           currencies=sorted(currencies)))
+    expires = datetime.datetime.now()+datetime.timedelta(days=15)
+    response.set_cookie("publication",publication,expires=expires)
+    response.set_cookie("city",city,expires=expires)
+    response.set_cookie("currency_from",currency_from,expires=expires)
+    response.set_cookie("currency_to",currency_to,expires=expires)
+    return response
+
+
 
 
 
@@ -100,7 +102,14 @@ def get_rates(frm,to):
     to_rate = parsed.get(to.upper())
     return (to_rate/frm_rate,parsed.keys())
 
-
+def get_value_with_fallback(key):
+    '''Function to handle retrieved cookies
+    its simply an ordereded loop-up'''
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
 
 
 if __name__ == '__main__':
