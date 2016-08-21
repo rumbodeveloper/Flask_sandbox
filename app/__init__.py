df32d14ee914f509c10a709df66fe40aee77d01f
rumbodeveloper <rumbo.developer@rumbo181.com>
2016-08-21 22:04:49 +0200

    DEBUG = False

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__,)
-    app.config['DEBUG']=True
+    #app.config['DEBUG']=True
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
